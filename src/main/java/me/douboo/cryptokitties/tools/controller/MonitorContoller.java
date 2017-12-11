96c3948766ffc08a3b4fa905fe2c160c39bdb9c6
卢衡 <15174834@qq.com>
2017-12-11 16:37:18 +0800

    search

--- src/main/java/me/douboo/cryptokitties/tools/controller/MonitorContoller.java
+++ src/main/java/me/douboo/cryptokitties/tools/controller/MonitorContoller.java
@@ -2,10 +2,12 @@ package me.douboo.cryptokitties.tools.controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -33,20 +35,41 @@ public class MonitorContoller {
 
 	@RequestMapping(path = "/data")
 	@ResponseBody
-	public JSONObject data(HttpServletRequest request, @RequestParam(required = false, defaultValue = "0") Integer offset, @RequestParam(required = false, defaultValue = "50") Integer limit, String sort, String order) {
+	public JSONObject data(HttpServletRequest request, String search, @RequestParam(required = false, defaultValue = "0") Integer offset, @RequestParam(required = false, defaultValue = "50") Integer limit, String sort, String order) {
 		request.setAttribute("connections", CommanderJob.commandQueue1.size());
 		JSONObject page = new JSONObject();
-		int total = jdbcTemplate.queryForObject("SELECT count(1) FROM t_auction a LEFT JOIN t_kitty k  ON a.`kitty_id` = k.`id` ", Integer.class);
+		List<Object> params = new ArrayList<>();
+		String sql = "SELECT count(1) FROM t_auction a LEFT JOIN t_kitty k  ON a.`kitty_id` = k.`id` ";
+		if (StringUtils.isNotEmpty(search)) {
+			sql += " where k.id = ?";
+			params.add(search);
+		}
+		int total = jdbcTemplate.queryForObject(sql, params.toArray(), Integer.class);
 		page.put("total", total);
 
-		String sql = "SELECT k.`status_cooldown_index`,k.`image_url`,k.`bio`,a.`current_price`,k.`id`,k.`kitty_name`,k.`generation`,k.`children_num`,k.`cattributes_sum`,k.`cattributes_avg`,k.`cattributes`,k.`cattributes_tags` FROM t_auction a LEFT JOIN t_kitty k  ON a.`kitty_id` = k.`id`  ";
-		if ("current_price".equals(sort)) {
-			sql += "order by " + sort + " " + order;
-		} else {
-			sql += "order by " + sort + " " + order + ",current_price asc";
+		params.clear();
+
+		sql = "SELECT k.`status_cooldown_index`,k.`image_url`,k.`bio`,a.`current_price`,k.`id`,k.`kitty_name`,k.`generation`,k.`children_num`,k.`cattributes_sum`,k.`cattributes_avg`,k.`cattributes`,k.`cattributes_tags` FROM t_auction a LEFT JOIN t_kitty k  ON a.`kitty_id` = k.`id`  ";
+
+		if (StringUtils.isNotEmpty(search)) {
+			sql += " where k.id = ?";
+			params.add(search);
 		}
-		sql += " limit " + offset + "," + limit;
-		List<JSONObject> rows = jdbcTemplate.query(sql, new RowMapper<JSONObject>() {
+
+		if ("current_price".equals(sort)) {
+			sql += "order by ? ?";
+			params.add(sort);
+			params.add(order);
+		} else {
+			sql += "order by ? ?,current_price asc";
+			params.add(sort);
+			params.add(order);
+		}
+		sql += " limit ?,? ";
+		params.add(offset);
+		params.add(limit);
+
+		List<JSONObject> rows = jdbcTemplate.query(sql, params.toArray(), new RowMapper<JSONObject>() {
 			@Override
 			public JSONObject mapRow(ResultSet rs, int rowNum) throws SQLException {
 				JSONObject item = new JSONObject();
